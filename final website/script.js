e6405b768992b1e617cbe64b38911af0699e6e68
Jacob Sulecki <97922961+JacobSulecki@users.noreply.github.com>
2023-08-09 05:17:52 -0500

    Add files via upload

---
+++ final website/script.js
@@ -0,0 +1,7 @@
+document.getElementById("nav").addEventListener("mouseover", function() {
+    document.getElementById("nav").style.backgroundColor = "rgb(201, 201, 201)";
+});
+
+document.getElementById("nav").addEventListener("mouseout", function() {
+    document.getElementById("nav").style.backgroundColor = "transparent";
+});
